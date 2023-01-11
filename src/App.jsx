ee087290fa4dccea7f05b038077c32ab0276e4a5
jajablinky <gmillar6@gmail.com>
2023-01-11 15:47:59 -0800

    initialize to restart on main app and removed css to start with clean boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,6 @@
 import React, { useState, useRef, useCallback } from "react";
 import useBookSearch from "./useBookSearch";
 
-function App() {
-  const [query, setQuery] = useState("");
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const observer = useRef();
-  const lastBookElementRef = useCallback((node) => {
-    console.log(node);
-  });
-
-  function handleSearch(e) {
-    setQuery(e.target.value);
-    setPageNumber(1);
-  }
-  const { books, hasMore, loading, error } = useBookSearch(query, pageNumber);
-  return (
-    <>
-      <input type="text" onChange={handleSearch}></input>
-      {books.map((book, index) => {
-        if (books.length === index + 1) {
-          return (
-            <div ref={lastBookElementRef} key={book}>
-              {book}
-            </div>
-          );
-        } else {
-          return <div key={book}>{book}</div>;
-        }
-      })}
-      <div>{loading && "Loading..."}</div>
-      <div>{error && "Error"}</div>
-    </>
-  );
-}
+function App() {}
 
 export default App;
